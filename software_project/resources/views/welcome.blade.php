b5ccd4154f4ce1a158ca2d23b4d6d99f9dd78ac2
Tariq Horan <N00202437@iadt.ie>
2022-02-27 20:03:14 +0000

    finished system design section

--- software_project/resources/views/welcome.blade.php
+++ software_project/resources/views/welcome.blade.php
@@ -19,55 +19,28 @@
 </head>
     <body class="antialiased">
    
-<section class="container-fluid p-0 m-0">			
-<div class="row text-center p-0 m-0">
-      <div class="col-12 mb-5 p-0 m-0">
-
-        <img class = "img-fluid" src="{{url('/images/homepage1.jpg')}}" alt="Image"/>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">BLAZAR</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <form class="d-flex ms-5">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
-  
-  
-  
-  
   </div>
 </nav>
-<img class = "img-fluid" src="{{url('/images/homepage2.jpg')}}" alt="Image"/>
-      </div>
-    </div>
-  </section>
 
 </body>
 </html>
