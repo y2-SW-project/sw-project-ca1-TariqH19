82e0a7ae8d334efc83fe49c721dfe5d605698b20
Tariq Horan <N00202437@iadt.ie>
2022-03-30 16:42:54 +0100

    I set up all the files needed for the clothing part of the site

--- software_projects/routes/web.php
+++ software_projects/routes/web.php
@@ -2,6 +2,9 @@
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\Admin\ShoeController as AdminShoeController;
 use App\Http\Controllers\User\ShoeController as UserShoeController;
+// this is for the clothing
+// use App\Http\Controllers\Admin\ClothingController as AdminClothingController;
+// use App\Http\Controllers\User\ClothingController as UserClothingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'welcome'])->name('welcome');
@@ -27,3 +30,32 @@ Route::post('/admin/shoes/store', [AdminShoeController::class, 'store'])->name('
 Route::get('/admin/shoes/{id}/edit', [AdminShoeController::class, 'edit'])->name('admin.shoes.edit');
 Route::put('/admin/shoes/{id}', [AdminShoeController::class, 'update'])->name('admin.shoes.update');
 Route::delete('/admin/shoes/{id}', [AdminShoeController::class, 'destroy'])->name('admin.shoes.destroy');
+
+
+
+// THIS IS FOR THE CLOTHING
+
+
+// Route::get('/', [PageController::class, 'welcome'])->name('welcome');
+// Route::get('/about', [PageController::class, 'about'])->name('about');
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('admin/home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
+// Route::get('user/home', [App\Http\Controllers\User\HomeController::class, 'index'])->name('user.home');
+
+// Route::get('/user/shoes/', [UserShoeController::class, 'index'])->name('user.shoes.index');
+// Route::get('/user/shoes/{id}', [UserShoeController::class, 'show'])->name('user.shoes.show');
+// Route::get('/user/shoes/{id}/buy', [UserShoeController::class, 'buy'])->name('user.shoes.buy');
+// Route::get('/user/shoes/{id}/bid', [UserShoeController::class, 'bid'])->name('user.shoes.bid');
+// Route::get('/user/shoes/{id}/sell', [UserShoeController::class, 'sell'])->name('user.shoes.sell');
+
+// Route::get('/admin/shoes/', [AdminShoeController::class, 'index'])->name('admin.shoes.index');
+// Route::get('/admin/shoes/create', [AdminShoeController::class, 'create'])->name('admin.shoes.create');
+// Route::get('/admin/shoes/{id}', [AdminShoeController::class, 'show'])->name('admin.shoes.show');
+// Route::post('/admin/shoes/store', [AdminShoeController::class, 'store'])->name('admin.shoes.store');
+// Route::get('/admin/shoes/{id}/edit', [AdminShoeController::class, 'edit'])->name('admin.shoes.edit');
+// Route::put('/admin/shoes/{id}', [AdminShoeController::class, 'update'])->name('admin.shoes.update');
+// Route::delete('/admin/shoes/{id}', [AdminShoeController::class, 'destroy'])->name('admin.shoes.destroy');
